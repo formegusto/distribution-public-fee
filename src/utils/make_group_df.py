a02fbaecc6a9fb9d962de305ee91f58ed21869a3
formegusto <formegusto@gmail.com>
2022-05-12 11:45:47 +0900

    update : adjust anomaly module test okay

--- src/utils/make_group_df.py
+++ src/utils/make_group_df.py
@@ -2,14 +2,18 @@ import numpy as np
 import pandas as pd
 
 
-def make_group_df(datas, kmeans, type="kmeans"):
-    kmeans.sorting()
+def make_group_df(datas, kmeans, _type="kmeans"):
+    if _type == "kmeans":
+        kmeans.sorting()
 
     group_df = pd.DataFrame(
         np.column_stack(
             [datas.columns.values, datas.sum(axis=0).round().astype("int")]),
         columns=['가구명', 'usage (kWh)']
     )
-    group_df['label'] = kmeans.labels_.astype("int")
+    if _type == "kmeans":
+        group_df['label'] = kmeans.labels_.astype("int")
+    else:
+        group_df['label'] = kmeans.groups_.astype("int")
 
     return group_df
