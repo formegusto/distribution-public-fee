6915d37d1e7c2e9066a45f75906a4244e50323e8
formegusto <formegusto@gmail.com>
2022-05-23 10:53:56 +0900

    update : SavingFeedback KMeans Process Comp

--- src/SavingFeedback/adjust_anomaly.py
+++ src/SavingFeedback/adjust_anomaly.py
@@ -29,4 +29,18 @@ def adjust_anomaly(self):
         new_labels = self.group['label'].values.copy()
         new_labels[chk] = predict_label
 
-        self.group['label'] = new_labels
+        # 순차적인 라벨 구성을 위한 조정
+        uni_labels = np.unique(new_labels)
+
+        _new_labels = np.zeros(new_labels.size) - 1
+        for idx, label in enumerate(uni_labels):
+            _new_labels[new_labels == label] = idx
+
+        _new_labels = _new_labels.astype("int")
+
+        self.group['label'] = _new_labels
+
+        self.kmeans.K = uni_labels.size
+        self.kmeans.clusters_ = self.kmeans.clusters_[:uni_labels.size]
+        self.kmeans.labels_ = _new_labels
+        self.kmeans.next_setting()
