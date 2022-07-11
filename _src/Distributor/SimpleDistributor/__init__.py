fe3478f4aa6c51bc2f4bfc2f1f76e18e6794ce7b
formegusto <formegusto@gmail.com>
2022-07-11 14:29:10 +0900

    이거 만들어야 함,,

--- _src/Distributor/SimpleDistributor/__init__.py
+++ _src/Distributor/SimpleDistributor/__init__.py
@@ -1,9 +1,19 @@
-from ..common import set_init
+from ..common import set_init, select_month
+from .set_cont import set_cont
+from .set_calc import set_calc
+from ...PublicAnalysis.BillTable import BillTable
 
 
 class SimpleDistributor:
     def __init__(self, file_path):
         self.file_path = file_path
 
+    def set_bill_table(self, public_percentage=30):
+        self.bill_table = BillTable(
+            analyzer=self, public_percentage=public_percentage)
+
 
 SimpleDistributor.set_init = set_init
+SimpleDistributor.select_month = select_month
+SimpleDistributor.set_cont = set_cont
+SimpleDistributor.set_calc = set_calc
--- _src/Distributor/common/__init__.py
+++ _src/Distributor/common/__init__.py
@@ -1,4 +1,5 @@
 from .set_init import set_init
+from .select_month import select_month
 
 __version__ = "1.0.0"
-__all__ = ["set_init"]
+__all__ = ["set_init", "select_month"]
